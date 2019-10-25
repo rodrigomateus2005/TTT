59997a3887b309a50180da67149ba4ddb2906387
Rodrigo Alves <rodrigo.alves@jnmoura.com.br>
2019-10-25 17:52:03 -0300

    Estaca Zero

--- Sismoura-Web-Controls/src/lib/diretivas/dev-extreme-grid-view.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DevExtremeGridViewDirective } from './dev-extreme-grid-view.directive';
-
-describe('DevExtremeGridViewDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DevExtremeGridViewDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Sismoura-Web-Controls/src/lib/diretivas/so-numeros.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SoNumerosDirective } from './so-numeros.directive';
-
-describe('SoNumerosDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SoNumerosDirective();
-    expect(directive).toBeTruthy();
-  });
-});
